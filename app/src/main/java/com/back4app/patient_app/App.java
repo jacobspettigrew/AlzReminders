57afe1d3b27e25c71aa465aa58ca8e55f3184d53
kc <kyungcheolkoh@gmail.com>
2020-10-27 18:35:19 +0900

    Simple database implemented
    only databse is implemented here

---
+++ app/src/main/java/com/back4app/patient_app/App.java
@@ -0,0 +1,28 @@
+package com.back4app.patient_app;
+
+import com.parse.GetCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import android.app.Application;
+import android.util.Log;
+import android.widget.EditText;
+
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                // if defined
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build()
+
+
+        );
+    }
+}
