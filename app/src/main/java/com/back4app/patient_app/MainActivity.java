27d6f1f0f25a53c416fed9535994e265b5fd34ef
kc <kyungcheolkoh@gmail.com>
2020-11-02 02:54:44 +0900

    connection to patient entity
    no unique id function is added yet

--- app/src/main/java/com/back4app/patient_app/MainActivity.java
+++ app/src/main/java/com/back4app/patient_app/MainActivity.java
@@ -1,13 +1,17 @@
 package com.back4app.patient_app;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseInstallation;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,20 +22,49 @@ public class MainActivity extends AppCompatActivity {
 
         // Save the current Installation to Back4App
         ParseInstallation.getCurrentInstallation().saveInBackground();
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("Storage", 0);
+        SharedPreferences.Editor editor = pref.edit();
 
+        TextView textView = findViewById(R.id.textView);
 
+//        editor.remove("uniqueId");
+//        editor.commit();
+        if(!pref.contains("uniqueId")){
 
+            ParseObject patient = new ParseObject("Patient");
+            patient.put("init", true);
+            String objectId;
+            patient.put("uniqueId", "uniqueIdssss");
+            patient.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if(e == null){
 
-
-        ParseUser.logInInBackground("john1", "password", new LogInCallback() {
-                    @Override
-                    public void done(ParseUser user, ParseException e) {
-                        EditText textView = findViewById(R.id.textView);
-
-                        textView.setText(user.getString("strings"));
+                    }
+                    else{
                     }
                 }
-        );
+            });
+            
+            objectId = patient.getString("uniqueId");
+            editor.putString("uniqueId", objectId);
+            textView.setText("objectId");
+            editor.commit();
+        }
+        else {
+            textView.setText(pref.getString("uniqueId", "didn't get unique id"));
+
+        }
+
+
+//        ParseUser.logInInBackground("john1", "password", new LogInCallback() {
+//                    @Override
+//                    public void done(ParseUser user, ParseException e) {
+//                        EditText textView = findViewById(R.id.textView);
+//                        textView.setText(user.getString("strings"));
+//                    }
+//                }
+//        );
 
 
 
